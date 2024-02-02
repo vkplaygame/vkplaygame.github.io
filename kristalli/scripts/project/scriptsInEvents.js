cf421154c4c74029d9c3824a24f0dbba4cd3e11f
vkplaygame <90629451+vkplaygame@users.noreply.github.com>
2024-02-02 22:56:36 +0300

    Add files via upload

---
+++ kristalli/scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+
+
+
+const scriptsInEvents = {
+
+	async GameEvents_Event2_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#915a99'
+	},
+
+	async MenuEvents_Event6_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#915a99'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
