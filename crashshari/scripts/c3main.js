b75875e0d8190808a8ddc19c70a210ef53677b19
vkplaygame <90629451+vkplaygame@users.noreply.github.com>
2025-01-11 00:57:27 +0300

    Add files via upload

---
+++ crashshari/scripts/c3main.js
@@ -0,0 +1,4 @@
+import "./c3runtime.js";
+import "./objRefTable.js";
+import "./project/sdk.js";
+import "./project/scriptsInEvents.js";
---
+++ crashshari/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async EventMenu_Event1_Act11(runtime, localVars)
+	{
+		window.Sonar.show({ adUnit: '{rewardedAdUnitName}' });
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
