a2ce4392126cc87405da449322223c57429e38a1
vkplaygame <90629451+vkplaygame@users.noreply.github.com>
2025-01-11 18:59:29 +0300

    Add files via upload

---
+++ crashshari/scripts/project/scriptsInEvents.js
@@ -0,0 +1,40 @@
+var adsonarScript = document.createElement('script');
+adsonarScript.src = 'https://static.sonartech.io/lib/1.0.0/sonar.js?appId=app_454e73a0'; // Замените на URL SDK Adsonar
+document.head.appendChild(adsonarScript);
+
+
+const scriptsInEvents = {
+
+	async EventMenu_Event1_Act12(runtime, localVars)
+	{
+		var adsonarScript = document.createElement('script');
+		adsonarScript.src = 'https://static.sonartech.io/lib/1.0.0/sonar.js?appId=app_454e73a0'; // Замените на URL SDK Adsonar
+		document.head.appendChild(adsonarScript);
+	},
+
+	async EventMenu_Event1_Act13(runtime, localVars)
+	{
+		adsonarScript.onload = function() {
+		    Adsonar.init({
+		        appId: 'app_454e73a0', // Замените на ваш App ID
+		        testMode: true // Включите тестовый режим
+		    });
+		};
+	},
+
+	async EventMenu_Event2_Act1(runtime, localVars)
+	{
+		function showInterstitial() {
+		    Adsonar.showInterstitial({
+		        adUnitId: 'interstitial', // Замените на ваш Ad Unit ID
+		        onAdClosed: function() {
+		            // Действия после закрытия рекламы
+		        }
+		    });
+		}
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
