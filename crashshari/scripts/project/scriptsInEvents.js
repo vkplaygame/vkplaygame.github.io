18e0f02347c8520089ba12c5af5fb2dc943945bd
vkplaygame <90629451+vkplaygame@users.noreply.github.com>
2025-01-13 23:40:51 +0300

    Add files via upload

---
+++ crashshari/scripts/project/scriptsInEvents.js
@@ -0,0 +1,42 @@
+// Замените {APP_ID} на ваш реальный идентификатор приложения
+var script = document.createElement('script');
+script.src = "https://static.sonartech.io/lib/1.0.0/sonar.js?appId=app_454e73a0&isDebug=true"; 
+document.head.appendChild(script);
+
+// Инициализация SDK, когда скрипт загружен
+script.onload = function() {
+    sonar.init({
+        appId: 'app_454e73a0', // Замените YOUR_APP_ID на ваш реальный ID
+        isDebug: true
+    });
+
+    // Здесь вы можете добавить код для управления рекламой
+};
+
+
+const scriptsInEvents = {
+
+	async EventGame_Event1_Act18(runtime, localVars)
+	{
+		window.Sonar.show({ adUnit: "interstitial" });
+	},
+
+	async EventGame_Event54_Act12(runtime, localVars)
+	{
+		window.Sonar.show({ adUnit: "interstitial" });
+	},
+
+	async EventMenu_Event1_Act11(runtime, localVars)
+	{
+		window.onload = function() {
+		         sonar.init({
+		             appId: '{app_454e73a0}',  // Убедитесь, что {APP_ID} заменен на ваш ID
+		             isDebug: true,
+		         });
+		     };
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
