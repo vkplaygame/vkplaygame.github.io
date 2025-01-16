6527dedc95d7e6424d99c250c8b304d93892e05c
vkplaygame <90629451+vkplaygame@users.noreply.github.com>
2025-01-16 13:46:29 +0300

    Add files via upload

---
+++ cvetboomtg/scripts/project/scriptsInEvents.js
@@ -0,0 +1,33 @@
+
+
+
+const scriptsInEvents = {
+
+	async EventGame_Event61_Act11(runtime, localVars)
+	{
+		// Попробуем инициировать и показать баннер
+		try {
+		    // Инициализация Adsgram и показ баннера
+		    await window.Adsgram.init({ blockId: "int-7201" }).show();
+		} catch (error) {
+		    // Обработка ошибок: ничего не делаем или выполняем другие действия
+		    console.error("Ошибка при показе рекламы:", error);
+		}
+	},
+
+	async EventMenu_Event2_Act1(runtime, localVars)
+	{
+		// Попробуем инициировать и показать баннер
+		try {
+		    // Инициализация Adsgram и показ баннера
+		    await window.Adsgram.init({ blockId: "int-7201" }).show();
+		} catch (error) {
+		    // Обработка ошибок: ничего не делаем или выполняем другие действия
+		    console.error("Ошибка при показе рекламы:", error);
+		}
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
