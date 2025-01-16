09182c0ed2d9ff431a132db847d9a7fa1add93c1
vkplaygame <90629451+vkplaygame@users.noreply.github.com>
2025-01-16 16:38:37 +0300

    Add files via upload

--- Cristaltads/scripts/project/scriptsInEvents.js
+++ Cristaltads/scripts/project/scriptsInEvents.js
@@ -37,6 +37,52 @@ const scriptsInEvents = {
 		    // Обработка ошибок: ничего не делаем или выполняем другие действия
 		    console.error("Ошибка при показе рекламы:", error);
 		}
+	},
+
+	async Emenu_Event1_Act12(runtime, localVars)
+	{
+		var script = document.createElement('script');
+		script.src = "https://w.tads.me/widget.js";
+		script.async = true;
+		document.head.appendChild(script);
+	},
+
+	async Emenu_Event1_Act13(runtime, localVars)
+	{
+		// Вставьте div для рекламы
+		const adContainer = document.createElement('div');
+		adContainer.id = 'tads-container-308'; // Замените YOUR_WIDGET_ID на ваш ID
+		document.body.appendChild(adContainer);
+	},
+
+	async Emenu_Event2_Act1(runtime, localVars)
+	{
+		document.addEventListener("DOMContentLoaded", function() {
+		    const initTadsWidget = (id, debug, onShowReward, onClickReward, onAdsNotFound) => {
+		        const adController = window.tads.init({ widgetId: id, debug: debug, onShowReward, onClickReward, onAdsNotFound });
+		        adController.loadAd()
+		            .then(() => adController.showAd())
+		            .catch((result) => {
+		                console.log(result);
+		            });
+		    };
+		
+		    // Callbacks
+		    const onShowRewardCallback = (result) => {
+		        console.log('Show ads, reward user:', result);
+		    };
+		
+		    const onClickRewardCallback = (result) => {
+		        console.log('Click on ad, reward user:', result);
+		    };
+		
+		    const onAdsNotFound = () => {
+		        console.log('Callback which calls if no ads found to show');
+		    };
+		
+		    // Init widget by Widget ID
+		    initTadsWidget('308', true, onShowRewardCallback, onClickRewardCallback, onAdsNotFound);
+		});
 	}
 
 };
